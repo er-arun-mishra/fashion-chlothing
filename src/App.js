238e38111250d0b5711a445378f714febe7083cd
unknown <Client@EBOMREN302NTD.ad.dspim.com>
2020-09-09 08:35:37 +0530

    created componenets for shoppingpage that contains collection items and its preview

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 
-const HatsPage = () =>(
-  <div>
-    <h1>Hats</h1>
-      {/* Hats{console.log(history)} */}
-</div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx'
+
 
 function App() {
   return <div>
     <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route  path='/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
       <Route/>
     </Switch>
 
