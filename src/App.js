38b8f6616968cf763e64d601e1ef6b8fb3f84d1f
er-arun-mishra <marun6592@gmail.com>
2020-09-16 10:02:07 +0530

    implemented firebase utils, included ability to store authenticated users into our firestore  database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from "./firebase/firebase.util";
+import { auth, createUserProfileDocument } from "./firebase/firebase.util";
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
@@ -9,8 +9,8 @@ import Header from './components/header/header.componenet';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
 
     this.state ={
       currentUser : null
@@ -20,11 +20,25 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(
-      user => {
-        this.setState({ currentUser:user});
-
-        console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot( snapshot => {
+          console.log(snapshot.data());
+          this.setState({
+            currentUser:{
+              id:snapshot.id,
+              ...snapshot.data()
+            }
+          });
+        });
+        console.log(this.state);
+      }else{
+        this.setState({
+          currentUser: null
+        });
+      }
+        
       }
     )
   }
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -13,6 +13,34 @@ const config = {
     measurementId: "G-C6TB9C2JFE"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) =>{
+    if(!userAuth) return;//This is basically to to check if user is sing out as if user sign out userAuth object that is recived is null.
+// if userAuth is not null then we need to query in firestore to chck if user document is already present in firestore or not
+    // console.log(firestore.doc('users/jkashfkjhfllj'));
+    // const userRef = firestore.doc('users/jkashfkjhfllj');    //Just to check what is returned by fake userId
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();//snapshot cnow contains the key uid that is the actual id that is passed.
+    console.log(snapShot);
+
+    if(!snapShot.exists){
+        const {displayName, email}  = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        }catch(error){
+            console.log(error.message);
+        }
+    }
+    return userRef;
+}
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
