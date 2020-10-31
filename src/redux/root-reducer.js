4602411168e284124d7bceaa80f1cad72d760ea5
er-arun-mishra <marun6592@gmail.com>
2020-10-31 19:48:14 +0530

    Added storing the data in the localstorage so that he checkout page still hass the data after refresh

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,27 @@
 import { combineReducers } from 'redux'; // combineReducers combines every reducers that we write in our code
 
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-export default combineReducers ({
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducers = combineReducers ({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducers);
+
+// initially we use to export combinereducers but now we will be using the persist-storage.
+// export default combineReducers ({
+//     user: userReducer,
+//     cart: cartReducer
+// });
 // Till now we have only written the root reducer and not brought the reducer into the state.
