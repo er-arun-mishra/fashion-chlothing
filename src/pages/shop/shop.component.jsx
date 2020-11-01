bf90406e23e7f73dea66df25f840ac602d042be4
er-arun-mishra <marun6592@gmail.com>
2020-11-01 15:09:24 +0530

    Added category Linking from the directory to the category page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,46 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/collection-preview.componenet';
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
+import {Route } from 'react-router-dom';
 
-        this.state={
-            collections: SHOP_DATA
-        };
-    }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-    render(){
-        const { collections } = this.state;
-        return(
-            <div className = 'shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const ShopPage = ({match}) =>(
+    <div className = 'shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={ `${match.path}/:collectionId` } component={ CollectionPage }></Route>
+    </div>
+)
 
-export default ShopPage;
+export default ShopPage
+
+
+
+
+// const mapStateToProps = createStructuredSelector({
+//     collections: selectCollections
+// })
+// The below code uses the shop data with state and the above one uses the shop data from selectors  i.e reducers
+// class ShopPage extends React.Component{
+//     constructor(props){
+//         super(props);
+
+//         this.state={
+//             collections: SHOP_DATA
+//         };
+//     }
+
+//     render(){
+//         const { collections } = this.state;
+//         return(
+//             <div className = 'shop-page'>
+//                 {
+//                     collections.map(({ id, ...otherCollectionProps }) => (
+//                         <CollectionPreview key={id} {...otherCollectionProps}/>
+//                     ))
+//                 }
+//             </div>
+//         )
+//     }
+// }
+
+// export default connect(mapStateToProps)(ShopPage);
